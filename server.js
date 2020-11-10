5ace14f77b54185a98bbcf2a3431651123303b2a
Berlen15 <belenpulidorivero13@gmail.com>
2020-11-10 17:14:44 +0000

    BD Configured

---
+++ server.js
@@ -0,0 +1,20 @@
+
+const express = require('express');
+const mongoose = require('mongoose');
+const UserModel = require('./api/users/users.model');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const app = express();
+const cors = require('cors');
+require('dotenv').config()
+
+mongoose.connect('mongodb+srv://admin:${process.env.MONGODB_ADMIN_PASSWORD}@cluster0.lpvxz.mongodb.net/onlyfriends?retryWrites=true&w=majority', 
+{ useNewUrlParser: true, useUnifiedTopology:  true });
+
+app.use(express.json())
+
+
+app.listen(5000, (err) => {
+    if (err) return console.log('ERROR: ', err);
+    console.log('Servidor corriendo en el 5000');
+})
