4b4c38e12ddec799b75c13066def9d4c8ade1fb2
Asier <asiertejera13@hotmail.com>
2020-11-17 18:09:49 +0000

    Fixing endpoint errors

--- api/users/users.router.js
+++ api/users/users.router.js
@@ -5,10 +5,10 @@ router.post('/register', controller.createOne )
 router.get('/:id', controller.getOneById);
 router.put('/:id/hobbies', controller.selectHobbies);     //put method only for first selection
 router.put('/:id/location', controller.selectLocation);   //put method only for first selection
-router.put('/:id', controller.selectGender);   //put method only for first selection
+router.put('/:id/gender', controller.selectGender);   //put method only for first selection
 router.patch('/:id/hobbies', controller.editHobbies);     //patch method only for modify hobbies
-router.patch('/:id', controller.editName);     //patch method only to modify my name
-router.patch('/:id', controller.editMail);     //patch method only to modify my email
-router.patch('/:id', controller.editGender);     //patch method only to modify my gender
+router.patch('/:id/name', controller.editName);     //patch method only to modify my name
+router.patch('/:id/email', controller.editMail);     //patch method only to modify my email
+router.patch('/:id/gender', controller.editGender);     //patch method only to modify my gender
 router.patch('/:id/location', controller.editLocation);     //patch method only to modify my location
 module.exports = router;
