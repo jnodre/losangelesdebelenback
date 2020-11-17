08ea7977b9c560a0ff7fd2a9830868b777af7de8
Asier <asiertejera13@hotmail.com>
2020-11-17 17:20:54 +0000

    Methods to modify your name, email and gender implemented

--- api/users/users.controller.js
+++ api/users/users.controller.js
@@ -6,6 +6,9 @@ module.exports.getOneById = getOneById;
 module.exports.selectHobbies = selectHobbies;
 module.exports.selectLocation = selectLocation;
 module.exports.editHobbies = editHobbies;
+module.exports.editName = editName;
+module.exports.editMail = editMail;
+module.exports.editGender = editGender;
 
 function getOneById(req, res) {
     const {
@@ -78,6 +81,66 @@ function editHobbies(req, res) {
         .then(async user => {
             if (user) {
                 user.hobbies = req.body.hobbies;
+                return user.save()
+                    .then(userEdited => {
+                        return res.json(userEdited);
+                    })
+            } else {
+                return res.status(400).send("That user doesnt exists ");
+            }
+        }).catch(e => res.status(500).json(e))
+}
+
+function editName(req, res) {
+    const {
+        id
+    } = req.params;
+    return userModel.findOne({
+            _id: id
+        })
+        .then(async user => {
+            if (user) {
+                user.name = req.body.name;
+                return user.save()
+                    .then(userEdited => {
+                        return res.json(userEdited);
+                    })
+            } else {
+                return res.status(400).send("That user doesnt exists ");
+            }
+        }).catch(e => res.status(500).json(e))
+}
+
+function editMail(req, res) {
+    const {
+        id
+    } = req.params;
+    return userModel.findOne({
+            _id: id
+        })
+        .then(async user => {
+            if (user) {
+                user.email = req.body.email;
+                return user.save()
+                    .then(userEdited => {
+                        return res.json(userEdited);
+                    })
+            } else {
+                return res.status(400).send("That user doesnt exists ");
+            }
+        }).catch(e => res.status(500).json(e))
+}
+
+function editGender(req, res) {
+    const {
+        id
+    } = req.params;
+    return userModel.findOne({
+            _id: id
+        })
+        .then(async user => {
+            if (user) {
+                user.gender = req.body.gender;
                 return user.save()
                     .then(userEdited => {
                         return res.json(userEdited);
--- api/users/users.router.js
+++ api/users/users.router.js
@@ -6,4 +6,7 @@ router.get('/:id', controller.getOneById);
 router.put('/:id/hobbies', controller.selectHobbies);     //put method only for first selection
 router.put('/:id/location', controller.selectLocation);   //put method only for first selection
 router.patch('/:id/hobbies', controller.editHobbies);     //patch method only for modify hobbies
+router.patch('/:id', controller.editName);     //patch method only to modify my name
+router.patch('/:id', controller.editMail);     //patch method only to modify my email
+router.patch('/:id', controller.editGender);     //patch method only to modify my gender
 module.exports = router;
