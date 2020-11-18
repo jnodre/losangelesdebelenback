2eb447d5b3ff0cafe43ae93aba8df7113128f723
Alejandro Sánchez <alesan.ca3@gmail.com>
2020-11-18 20:16:10 +0000

    groups model added

---
+++ api/groups/groups.model.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const groupsSchema = mongoose.Schema({
+    group : {
+        type: String,
+        unique: true,
+        required: true
+    },
+    members : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'user'
+    }
+})
+
+const groupsModel = mongoose.model('groups', groupsSchema)
+
+module.exports = groupsModel;
