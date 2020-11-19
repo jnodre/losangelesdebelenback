8bbf0883f33e7cdd0ffea8af8a3a0fef15e4a9a0
Asier <asiertejera13@hotmail.com>
2020-11-19 10:29:15 +0000

    Get implemented (need to fix errors)

--- api/groups/groups.controller.js
+++ api/groups/groups.controller.js
@@ -1,9 +1,22 @@
 const groupsModel = require("./groups.model");
 
 module.exports.createGroup = createGroup;
+module.exports.getOneGroupById = getOneGroupById;
 
 function createGroup(req, res){
     return groupsModel.create(req.body)
     .then(u => res.json(u))
     .catch(e => res.status(500).json(e))
 }
+
+function getOneGroupById(req, res) {
+    const {
+      id
+    } = req.body;
+    return groupsModel
+      .findOne({
+        _id: id
+      })
+      .then(u => res.json(u))
+      .catch(e => res.status(500).json(e))
+  }
--- api/groups/groups.router.js
+++ api/groups/groups.router.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const controller = require('./groups.controller');
 
 router.post('creategroup', controller.createGroup);
+router.get('/groups', controller.getOneGroupById);
 
 module.exports = router;
