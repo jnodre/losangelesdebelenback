b3eceb74e7e2bbe7eb22f7745522ba533199d191
blueC4P <osamag4g@gmail.com>
2020-11-20 12:12:11 +0100

    fixed cookies things

--- server.js
+++ server.js
@@ -42,10 +42,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(cors())
-Response.Headers.AddCookies(new Cookie[]{
-  new Cookie("WorkingCookie", cookieValue) { Path="/" }, 
-  new Cookie("NotWorkingCookie", cookieValue) 
-})
 app.use(express.json())
 app.use('/users', usersRouter)
 
