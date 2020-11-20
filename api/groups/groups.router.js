227f0bbe31f71fa633d3c376999bc20180313358
Alejandro Sánchez <alesan.ca3@gmail.com>
2020-11-20 20:33:59 +0000

    get All groups router added

--- api/groups/groups.router.js
+++ api/groups/groups.router.js
@@ -5,5 +5,6 @@ router.post('/creategroup', controller.createGroup);
 router.get('/:id', controller.getGroup);
 router.put('/:id/members', controller.addMember);
 router.get('/:id/members' , controller.getAllMembers);
+router.get('/' , controller.getAllGroups);
 
 module.exports = router;
