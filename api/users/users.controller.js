9fa6d00012671c10370853bf4f4f3ef0e72c93e8
Alejandro Sánchez <alesan.ca3@gmail.com>
2020-11-21 21:40:25 +0000

    patches added

--- api/users/users.controller.js
+++ api/users/users.controller.js
@@ -1,5 +1,4 @@
 const bcrypt = require('bcrypt');
-const groupsModel = require('../groups/groups.model');
 const userModel = require("./users.model");
 
 module.exports.createOne = createAcc;
@@ -10,13 +9,11 @@ module.exports.editHobbies = editHobbies;
 module.exports.getAllUsers = getAllUsers;
 module.exports.giveMatch = giveMatch;
 module.exports.getFriends = getFriends;
-module.exports.getUsers = getUsers;
-
-function getUsers(req, res) {
-  return userModel.find({}).then(response => {
-    res.json(response)
-  }).catch(e => res.status(500).json(e));
-}
+module.exports.editName = editName;
+module.exports.editMail = editMail;
+module.exports.editGender = editGender;
+module.exports.editLocation = editLocation;
+module.exports.editPassword = editPassword;
 
 function getOneById(req, res) {
   const {
@@ -50,19 +47,8 @@ function selectHobbies(req, res) {
         user.hobbies = newHobbies;
         return user.save()
           .then(userEdited => {
-            userEdited.hobbies.forEach(element => {
-              groupsModel.findOne({
-                group : element
-              })
-              .then(g =>{
-                g.members.push(id);
-                g.save();
-                res.json(g);
-              })
-              .catch(e => res.status(500).json(e))           
-            });
-            res.json(userEdited);
-          }).catch(e => res.status(500).json(e))
+            return res.json(userEdited);
+          })
       } else {
         return res.status(400).send("That user doesnt exists ");
       }
@@ -100,33 +86,12 @@ function editHobbies(req, res) {
       _id: id
     })
     .then(async user => {
-      user.hobbies.forEach(element =>{
-        groupsModel.findOne({
-          group : element
-        }).then(g=> {
-          g.members = g.members.filter(user => user._id != id);
-          g.save();
-        }).catch(e => res.status(500).json(e))
-      })
-      if (user) { 
+      if (user) {
         user.hobbies = req.body.hobbies;
         return user.save()
           .then(userEdited => {
-            userEdited.hobbies.forEach(element => {
-              groupsModel.findOne({
-                group : element
-              })
-              .then(g => {
-                if(!g.members.includes(id)){
-                  g.members.push(id);
-                  g.save();
-                  return res.json(g);
-                }else{
-                  res.status(500).json("The user exist inside group");
-                }
-              })
-            }).catch(e => res.status(500).json(e))
-          }).catch(e => res.status(500).json(e))
+            return res.json(userEdited);
+          })
       } else {
         return res.status(400).send("That user doesnt exists ");
       }
@@ -189,13 +154,114 @@ function giveMatch(req, res) {
 }
 
 async function getFriends(req, res) {
-  const {id} = req.params;
+  const {
+    id
+  } = req.params;
   return userModel.findOne({
       _id: id
     })
     .populate('friends')
     .then(user => {
-        res.json(user.friends);
-      });
+      res.json(user.friends);
+    });
 }
 
+function editName(req, res) {
+  const {
+    id
+  } = req.params;
+  return userModel.findOne({
+      _id: id
+    })
+    .then(async user => {
+      if (user) {
+        user.name = req.body.name;
+        return user.save()
+          .then(userEdited => {
+            return res.json(userEdited);
+          })
+      } else {
+        return res.status(400).send("That user doesnt exists ");
+      }
+    }).catch(e => res.status(500).json(e))
+}
+
+function editMail(req, res) {
+  const {
+    id
+  } = req.params;
+  return userModel.findOne({
+      _id: id
+    })
+    .then(async user => {
+      if (user) {
+        user.email = req.body.email;
+        return user.save()
+          .then(userEdited => {
+            return res.json(userEdited);
+          })
+      } else {
+        return res.status(400).send("That user doesnt exists ");
+      }
+    }).catch(e => res.status(500).json(e))
+}
+
+function editGender(req, res) {
+  const {
+    id
+  } = req.params;
+  return userModel.findOne({
+      _id: id
+    })
+    .then(async user => {
+      if (user) {
+        user.gender = req.body.gender;
+        return user.save()
+          .then(userEdited => {
+            return res.json(userEdited);
+          })
+      } else {
+        return res.status(400).send("That user doesnt exists ");
+      }
+    }).catch(e => res.status(500).json(e))
+}
+
+function editLocation(req, res) {
+  const {
+    id
+  } = req.params;
+  return userModel.findOne({
+      _id: id
+    })
+    .then(async user => {
+      if (user) {
+        user.location = req.body.location;
+        return user.save()
+          .then(userEdited => {
+            return res.json(userEdited);
+          })
+      } else {
+        return res.status(400).send("That user doesnt exists ");
+      }
+    }).catch(e => res.status(500).json(e))
+}
+
+function editPassword(req, res) {
+  const {
+    id
+  } = req.params;
+  return userModel.findOne({
+      _id: id
+    })
+    .then(async user => {
+      if (user) {
+        user.password = bcrypt.hashSync(req.body.password, 10);
+        return user.save()
+          .then(userEdited => {
+            return res.json(userEdited);
+          })
+      } else {
+        return res.status(400).send("That user doesnt exists ");
+      }
+    }).catch(e => res.status(500).json(e))
+}
