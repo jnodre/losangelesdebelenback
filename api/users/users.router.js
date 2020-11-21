ea894338bebf662d142537ebd638fa7f8f2a0eef
Alejandro Sánchez <alesan.ca3@gmail.com>
2020-11-21 21:41:55 +0000

    patch added

--- api/users/users.router.js
+++ api/users/users.router.js
@@ -1,13 +1,15 @@
-const router = require('express').Router()
-const controller = require('./users.controller');
+const router = require("express").Router();
+const controller = require("./users.controller");
 
-router.post('/register', controller.createOne )
-router.get('/:id', controller.getOneById);
-router.get('/:id/people', controller.getAllUsers);
-router.get('/:id/friends', controller.getFriends);
-router.patch('/:id/people', controller.giveMatch);
-router.put('/:id/hobbies', controller.selectHobbies);     //put method only for first selection
-router.put('/:id/location', controller.selectLocation);   //put method only for first selection
-router.patch('/:id/hobbies', controller.editHobbies);     //patch method only for modify hobbies
-router.get('/' , controller.getUsers);
+router.post("/register", controller.createOne);
+router.get("/:id", controller.getOneById);
+router.get("/:id/people", controller.getAllUsers);
+router.get("/:id/friends", controller.getFriends);
+router.patch("/:id/people", controller.giveMatch);
+router.put("/:id/hobbies", controller.selectHobbies); //put method only for first selection
+router.put("/:id/location", controller.selectLocation); //put method only for first selection
+router.patch("/:id/hobbies", controller.editHobbies); //patch method only for modify hobbies
+router.patch("/:id/password", controller.editPassword); //patch method only to modify my location
+
+router.get("/", controller.getUsers);
 module.exports = router;
